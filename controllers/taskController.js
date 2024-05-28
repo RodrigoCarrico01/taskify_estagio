d193c077dd7598bccf51b19b47b4643bb41d9ced
RodrigoCarrico01 <mr.rodrigocarrico@gmail.com>
2024-05-28 13:06:57 +0100

    Estou a puxar as tarefas mas só aparecem 10

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,22 +1,42 @@
 // controllers/taskController.js
 const axios = require('axios');
+const { getAuthorizationToken } = require('../utils/authFormaloo');
 
 exports.getTasks = async (req, res) => {
-  const apiKey = "key_gAAAAABmBbnkRunYWIDY6NMDGwuLnQzbnSpyzXIT4-s_ASTHMFrQ1c_W1zB-EmW1vPX4MLy39kO7SQpOZHntlhjv-hojbGAfuWkgW5k6-7rYLio8dVyU5TpfXQskcQgOeAsk1sOikYQtsxVgwkrtY0iKsLkmX3Romw==";
-  const secretKey = "TnVMTnRTcnhqdHhVb0EwOWdoM2dzdXMySTFqaW9QMmIweGFiaThHYTpoeTY3YjVia3BOTUxjUjNOa1RrSG1lY094djhoS2JEcnM5d0d5RWhSMGNnYVEzajFyeWtZSDhtTFhydnBWSXFhcFc3V3FLN3VaR3ZobG9xT0ZUMTJZRjhTSjhKamd6cUJPMTl1RmR0bVo5dzk1MEljenZBb1FGTW9NcDhqTUVFeg==";
+  const apiKey = 'key_gAAAAABmBbnkRunYWIDY6NMDGwuLnQzbnSpyzXIT4-s_ASTHMFrQ1c_W1zB-EmW1vPX4MLy39kO7SQpOZHntlhjv-hojbGAfuWkgW5k6-7rYLio8dVyU5TpfXQskcQgOeAsk1sOikYQtsxVgwkrtY0iKsLkmX3Romw==';
 
   try {
-    const response = await axios.get('https://api.formaloo.net/v3/forms/m3fK0cg3/rows', {
-      headers: {
-        'X-API-Key': apiKey,
-        'Authorization': `JWT ${secretKey}`,
-        'Content-Type': 'application/json',
-      },
-    });
+    let authorizationToken = await getAuthorizationToken();
 
-    const tasks = response.data.data.rows;
+    let tasks = [];
+    let nextUrl = 'https://api.formaloo.net/v3/forms/m3fK0cg3/rows';
+    
+    while (nextUrl) {
+      try {
+        const response = await axios.get(nextUrl, {
+          headers: {
+            'x-api-key': apiKey,
+            'Authorization': `JWT ${authorizationToken}`,
+            'Content-Type': 'application/json',
+          },
+        });
+
+        tasks = tasks.concat(response.data.data.rows);
+
+        // Update nextUrl to fetch the next set of results if available
+        nextUrl = response.data.meta && response.data.meta.next ? response.data.meta.next : null;
+      } catch (error) {
+        if (error.response && error.response.status === 401) {
+          // If we get a 401 error, refresh the authorization token and retry the request
+          authorizationToken = await getAuthorizationToken();
+        } else {
+          throw error;
+        }
+      }
+    }
+
     res.render('tasks', { tasks });
   } catch (error) {
-    res.status(400).send(error.message);
+    res.status(400).send('Erro ao obter tarefas: ' + error.message);
   }
 };
---
+++ utils/authFormaloo.js
@@ -0,0 +1,26 @@
+// utils/authFormaloo.js
+const axios = require('axios');
+
+const getAuthorizationToken = async () => {
+  const url = 'https://api.formaloo.net/v1.0/oauth2/authorization-token/';
+  const payload = { grant_type: 'client_credentials' };
+  const secretKey = 'TnVMTnRTcnhqdHhVb0EwOWdoM2dzdXMySTFqaW9QMmIweGFiaThHYTpoeTY3YjVia3BOTUxjUjNOa1RrSG1lY094djhoS2JEcnM5d0d5RWhSMGNnYVEzajFyeWtZSDhtTFhydnBWSXFhcFc3V3FLN3VaR3ZobG9xT0ZUMTJZRjhTSjhKamd6cUJPMTl1RmR0bVo5dzk1MEljenZBb1FGTW9NcDhqTUVFeg==';
+
+  try {
+    const response = await axios.post(url, payload, {
+      headers: {
+        Authorization: `Basic ${secretKey}`
+      }
+    });
+
+    if (response.status === 200) {
+      return response.data.authorization_token;
+    } else {
+      throw new Error('Erro ao obter o token de autorização: ' + JSON.stringify(response.data));
+    }
+  } catch (error) {
+    throw new Error('Erro ao obter o token de autorização: ' + error.message);
+  }
+};
+
+module.exports = { getAuthorizationToken };
