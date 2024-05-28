aeae7f3b320d6d4283426b87e5b4abf8ebb4cfe0
RodrigoCarrico01 <mr.rodrigocarrico@gmail.com>
2024-05-28 14:34:13 +0100

    Está a puxar todas as tarefas

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -3,13 +3,14 @@ const { getAuthorizationToken } = require('../utils/authFormaloo');
 
 exports.getTasks = async (req, res) => {
   const apiKey = 'key_gAAAAABmBbnkRunYWIDY6NMDGwuLnQzbnSpyzXIT4-s_ASTHMFrQ1c_W1zB-EmW1vPX4MLy39kO7SQpOZHntlhjv-hojbGAfuWkgW5k6-7rYLio8dVyU5TpfXQskcQgOeAsk1sOikYQtsxVgwkrtY0iKsLkmX3Romw==';
+  const itemsPerPage = 20;
+  const currentPage = parseInt(req.query.page) || 1;
 
   try {
     let authorizationToken = await getAuthorizationToken();
-
     let tasks = [];
-    let nextUrl = 'https://api.formaloo.net/v3/forms/m3fK0cg3/rows?page_size=100'; // Ajustar o tamanho da página para 100 ou o máximo permitido pela API
-    
+    let nextUrl = `https://api.formaloo.net/v3/forms/m3fK0cg3/rows?page_size=max`;
+
     while (nextUrl) {
       try {
         const response = await axios.get(nextUrl, {
@@ -21,12 +22,9 @@ exports.getTasks = async (req, res) => {
         });
 
         tasks = tasks.concat(response.data.data.rows);
-
-        // Update nextUrl to fetch the next set of results if available
         nextUrl = response.data.meta && response.data.meta.next ? response.data.meta.next : null;
       } catch (error) {
         if (error.response && error.response.status === 401) {
-          // If we get a 401 error, refresh the authorization token and retry the request
           authorizationToken = await getAuthorizationToken();
         } else {
           throw error;
@@ -34,7 +32,15 @@ exports.getTasks = async (req, res) => {
       }
     }
 
-    res.render('tasks', { tasks });
+    const totalTasks = tasks.length;
+    const totalPages = Math.ceil(totalTasks / itemsPerPage);
+    const paginatedTasks = tasks.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
+
+    res.render('tasks', {
+      tasks: paginatedTasks,
+      currentPage,
+      totalPages
+    });
   } catch (error) {
     res.status(400).send('Erro ao obter tarefas: ' + error.message);
   }
